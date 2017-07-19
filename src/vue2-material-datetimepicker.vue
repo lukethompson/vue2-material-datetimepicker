dfea1219fd3051ab6b98e5bb4d4607fc50985526
Luke Thompson <1659140+lukethompson@users.noreply.github.com>
2017-07-19 11:34:59 +1000

    Update css prefix

--- src/vue2-material-datetimepicker.vue
+++ src/vue2-material-datetimepicker.vue
@@ -1,71 +1,71 @@
 <template>
-  <div class="vdp-datepicker">
-    <div class="vdp-input-container">
-      <input type="text" @click="show('picker')" @focus="show('picker')" :value="date.time.format('DD/MM/YYYY')" :title="option.inputTitle" class="vdp-input" readonly="readonly" />
+  <div class="vmdtp-datepicker">
+    <div class="vmdtp-input-container">
+      <input type="text" @click="show('picker')" @focus="show('picker')" :value="date.time.format('DD/MM/YYYY')" :title="option.inputTitle" class="vmdtp-input" readonly="readonly" />
     </div>
-    <div class="vdp-overlay" v-if="showPicker.picker" @click="overlayClick($event)">
-      <div class="vdp-dialog">
-        <div class="vdp-dialog-header">
-          <div class="vdp-dialog-title">
-            <span class="vdp-selected-year" @click="show('year')">{{ titleMoment.format('YYYY') }}</span>
-            <span class="vdp-selected-date" @click="setDisplayMoment(titleMoment)">{{ titleMoment.format('dddd') }}, {{ titleMoment.format('MMM') }} {{ titleMoment.date() }}</span>
+    <div class="vmdtp-overlay" v-if="showPicker.picker" @click="overlayClick($event)">
+      <div class="vmdtp-dialog">
+        <div class="vmdtp-dialog-header">
+          <div class="vmdtp-dialog-title">
+            <span class="vmdtp-selected-year" @click="show('year')">{{ titleMoment.format('YYYY') }}</span>
+            <span class="vmdtp-selected-date" @click="setDisplayMoment(titleMoment)">{{ titleMoment.format('dddd') }}, {{ titleMoment.format('MMM') }} {{ titleMoment.date() }}</span>
           </div>
         </div>
-        <div class="vdp-dialog-content">
-          <div class="vdp-picker-container" v-if="showPicker.year">
-            <div class="vdp-picker">
-              <div class="vdp-date-list" ref="yearList">
+        <div class="vmdtp-dialog-content">
+          <div class="vmdtp-picker-container" v-if="showPicker.year">
+            <div class="vmdtp-picker">
+              <div class="vmdtp-date-list" ref="yearList">
                 <ul>
                   <li class="year" v-for="(year,index) in years" :key="index" @click="setYear(year.value)" :class="{'checked':year.checked}" ref="year">{{ year.display }}</li>
                 </ul>
               </div>
             </div>
           </div>
-          <div class="vdp-picker-container" v-if="showPicker.month">
-            <div class="vdp-picker">
-              <div class="vdp-date-list" ref="monthList">
+          <div class="vmdtp-picker-container" v-if="showPicker.month">
+            <div class="vmdtp-picker">
+              <div class="vmdtp-date-list" ref="monthList">
                 <ul>
                   <li class="month" v-for="(month,index) in months" :key="index" @click="setMonth(month.value)" :class="{'checked':month.checked}" ref="month">{{ month.display }}</li>
                 </ul>
               </div>
             </div>
           </div>
-          <div class="vdp-picker-container" v-if="showPicker.day">
-            <div class="vdp-month-selection">
-              <div class="vdp-previous" @click="previousMonth()">«</div>
-              <div class="vdp-month-selection-month" @click="show('month')">
+          <div class="vmdtp-picker-container" v-if="showPicker.day">
+            <div class="vmdtp-month-selection">
+              <div class="vmdtp-previous" @click="previousMonth()">«</div>
+              <div class="vmdtp-month-selection-month" @click="show('month')">
                 {{ displayMoment.format('MMM') + ' ' + displayMoment.format('YYYY') }}
               </div>
-              <div class="vdp-next" @click="nextMonth()">»</div>
+              <div class="vmdtp-next" @click="nextMonth()">»</div>
             </div>
-            <div class="vdp-datepicker">
-              <div class="vdp-weekday-labels">
+            <div class="vmdtp-datepicker">
+              <div class="vmdtp-weekday-labels">
                 <ul>
                   <li v-for="weekLabel in weekLabels">{{ weekLabel }}</li>
                 </ul>
               </div>
-              <div class="vdp-day" v-for="(day,index) in days" @click="setSelectedMoment(day)" :key="index" :class="{'checked':day.checked,'unavailable':day.unavailable,'passive': !(day.inMonth)}">
+              <div class="vmdtp-day" v-for="(day,index) in days" @click="setSelectedMoment(day)" :key="index" :class="{'checked':day.checked,'unavailable':day.unavailable,'passive': !(day.inMonth)}">
                 {{ day.value }}
               </div>
             </div>
           </div>
-          <div class="vdp-picker-container" v-if="showPicker.time">
-            <div class="vdp-picker">
-              <div class="vdp-date-list vdp-hours">
+          <div class="vmdtp-picker-container" v-if="showPicker.time">
+            <div class="vmdtp-picker">
+              <div class="vmdtp-date-list vmdtp-hours">
                 <ul>
                   <li v-for="hour in hours">{{ hour.value }}</li>
                 </ul>
               </div>
-              <div class="vdp-date-list vdp-minutes">
+              <div class="vmdtp-date-list vmdtp-minutes">
                 <ul>
                   <li v-for="minute in minutes">{{ minute.value }}</li>
                 </ul>
               </div>
             </div>
           </div>
-          <div class="vdp-dialog-footer">
-            <span class="vdp-btn" @click="close()">Cancel</span>
-            <span class="vdp-btn" @click="save()">OK</span>
+          <div class="vmdtp-dialog-footer">
+            <span class="vmdtp-btn" @click="close()">Cancel</span>
+            <span class="vmdtp-btn" @click="save()">OK</span>
           </div>
         </div>
       </div>
@@ -149,7 +149,7 @@
         this.displayMoment = moment(this.displayMoment).subtract(1, 'month')
       },
       overlayClick (event) {
-        if (event.target.className === 'vdp-overlay') {
+        if (event.target.className === 'vmdtp-overlay') {
           this.close()
         }
       },
@@ -359,75 +359,74 @@
 </script>
 
 <style scoped>
-  .vdp-datepicker { display: inline-block; }
-  .vdp-overlay { height: 100vh; left: 0; position: fixed; top: 0; width: 100vw; z-index: 998;
+  .vmdtp-datepicker { display: inline-block; }
+  .vmdtp-overlay { height: 100vh; left: 0; position: fixed; top: 0; width: 100vw; z-index: 998;
     -webkit-animation: fadein 0.5s;
     -moz-animation: fadein 0.5s;
     -ms-animation: fadein 0.5s;
     -o-animation: fadein 0.5s;
     animation: fadein 0.5s;
   }
-  .vdp-dialog { background: #fff; box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.2); display: block; font-family: 'Roboto'; font-weight: 400; left: 50%; max-width: 100%; position: absolute; top: 30%; width: 400px; z-index: 999;
+  .vmdtp-dialog { background: #fff; box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.2); display: block; font-family: 'Roboto'; font-weight: 400; left: 50%; max-width: 100%; position: absolute; top: 30%; width: 400px; z-index: 999;
     -webkit-transform: translate(-50%, -50%);
     -ms-transform: translate(-50%, -50%);
     transform: translate(-50%, -50%);
   }
-  .vdp-dialog-header { background: #3F51B5; color: #fff; height: 120px; text-align: center; }
-  .vdp-dialog-title { box-sizing: border-box; display: inline-block; font-size: 16px; height: 100%; padding: 30px 0; text-align: left; }
-  .vdp-dialog-content { box-sizing: border-box; width: 100%; }
-  .vdp-dialog-footer { padding: 10px 25px 20px 25px; text-align: right; }
+  .vmdtp-dialog-header { background: #3F51B5; color: #fff; height: 120px; text-align: center; }
+  .vmdtp-dialog-title { box-sizing: border-box; display: inline-block; font-size: 16px; height: 100%; padding: 30px 0; text-align: left; }
+  .vmdtp-dialog-content { box-sizing: border-box; width: 100%; }
+  .vmdtp-dialog-footer { padding: 10px 25px 20px 25px; text-align: right; }
 
-  .vdp-picker-container { box-sizing: border-box; height: 320px; overflow: hidden; }
-  .vdp-picker { height: 100%; }
-  .vdp-date-list { height: 100%; overflow-y: scroll; }
-  .vdp-date-list ul { list-style: none; margin-top: 0; padding: 0; text-align: center; }
-  .vdp-date-list ul li { cursor: pointer; padding: 10px 0; }
-  .vdp-date-list ul li:hover { background: #e0e0e0; }
+  .vmdtp-picker-container { box-sizing: border-box; height: 320px; overflow: hidden; }
+  .vmdtp-picker { height: 100%; }
+  .vmdtp-date-list { height: 100%; overflow-y: scroll; }
+  .vmdtp-date-list ul { list-style: none; margin-top: 0; padding: 0; text-align: center; }
+  .vmdtp-date-list ul li { cursor: pointer; padding: 10px 0; }
+  .vmdtp-date-list ul li:hover { background: #e0e0e0; }
 
-  .vdp-btn { cursor: pointer; padding: 0 10px; }
+  .vmdtp-btn { cursor: pointer; padding: 0 10px; }
   
-  .vdp-datepicker { padding: 0 25px; }
+  .vmdtp-datepicker { padding: 0 25px; }
   
-  .vdp-month-selection { font-size: 0; height: 30px; line-height: 30px; margin: 25px 0; padding: 0 12px; }
-  .vdp-previous,
-  .vdp-next { color: #fff; display: inline-block; cursor: pointer; height: 100%; overflow: hidden; position: relative; text-indent: -300px; width: 15% !important; }
-  .vdp-previous:after,
-  .vdp-next:after { background: #000; content: ''; height: 2px; position: absolute; line-height: 0; margin-top: 6px; right: 0; text-align: center; top: 50%; width: 20px;
+  .vmdtp-month-selection { font-size: 0; height: 30px; line-height: 30px; margin: 25px 0; padding: 0 12px; }
+  .vmdtp-previous,
+  .vmdtp-next { color: #fff; display: inline-block; cursor: pointer; height: 100%; overflow: hidden; position: relative; text-indent: -300px; width: 15% !important; }
+  .vmdtp-previous:after,
+  .vmdtp-next:after { background: #000; content: ''; height: 2px; position: absolute; line-height: 0; margin-top: 6px; right: 0; text-align: center; top: 50%; width: 20px;
     -webkit-transform: rotate(-45deg);
     -moz-transform: rotate(-45deg);
     transform: rotate(-45deg);
   }
-  .vdp-previous:before,
-  .vdp-next:before { background: #000; content: ''; height: 2px; line-height: 0; margin-top: -7px; position: absolute; right: 0; text-align: center; top: 50%; width: 20px;
+  .vmdtp-previous:before,
+  .vmdtp-next:before { background: #000; content: ''; height: 2px; line-height: 0; margin-top: -7px; position: absolute; right: 0; text-align: center; top: 50%; width: 20px;
     -webkit-transform: rotate(45deg);
     -moz-transform: rotate(45deg);
     transform: rotate(45deg);
   }
-  .vdp-month-selection-month { cursor: pointer; display: inline-block; font-size: 18px; text-align: center; vertical-align: top; width: 70%; }
-  .vdp-previous::after { left: 0; right: auto;
+  .vmdtp-month-selection-month { cursor: pointer; display: inline-block; font-size: 18px; text-align: center; vertical-align: top; width: 70%; }
+  .vmdtp-previous::after { left: 0; right: auto;
     -webkit-transform: rotate(45deg);
     -moz-transform: rotate(45deg);
     transform: rotate(45deg);
   }
-  .vdp-previous::before { left: 0; right: auto;
+  .vmdtp-previous::before { left: 0; right: auto;
     -webkit-transform: rotate(-45deg);
     -moz-transform: rotate(-45deg);
     transform: rotate(-45deg);
   }
-  .vdp-weekday-labels { }
-  .vdp-weekday-labels ul { list-style: none; margin: 0; padding-left: 0; text-align: center; }
-  .vdp-weekday-labels ul li { color: #000; display: inline-block; font-weight: bold; text-align: center; width: 14.2%; }
-  .vdp-day { color: #000; cursor: pointer; display: inline-block; height: 34px; line-height: 34px; padding: 0; vertical-align: middle; text-align: center; width: 14.2%; }
-  .vdp-day.checked { background: #F50057; color: #FFF !important; border-radius: 3px; }
-  .vdp-day.checked:hover { background: #FF4F8E; }
-  .vdp-day.passive { color: #bbb; }
-  .vdp-day:hover { background: #eaeaea; }
+  .vmdtp-weekday-labels ul { list-style: none; margin: 0; padding-left: 0; text-align: center; }
+  .vmdtp-weekday-labels ul li { color: #000; display: inline-block; font-weight: bold; text-align: center; width: 14.2%; }
+  .vmdtp-day { color: #000; cursor: pointer; display: inline-block; height: 34px; line-height: 34px; padding: 0; vertical-align: middle; text-align: center; width: 14.2%; }
+  .vmdtp-day.checked { background: #F50057; color: #FFF !important; border-radius: 3px; }
+  .vmdtp-day.checked:hover { background: #FF4F8E; }
+  .vmdtp-day.passive { color: #bbb; }
+  .vmdtp-day:hover { background: #eaeaea; }
   .month.checked { background: #F50057; color: #FFF !important; border-radius: 3px; }
   .month.checked:hover { background: #FF4F8E; }
   .year.checked { background: #F50057; color: #FFF !important; border-radius: 3px; }
   .year.checked:hover { background: #FF4F8E; }
-  .vdp-selected-date { cursor: pointer; font-size: 28px; }
-  .vdp-selected-year { cursor: pointer; display: block; font-weight: 200; margin-bottom: 5px; }
+  .vmdtp-selected-date { cursor: pointer; font-size: 28px; }
+  .vmdtp-selected-year { cursor: pointer; display: block; font-weight: 200; margin-bottom: 5px; }
   ::-webkit-scrollbar { width: 2px; }
   ::-webkit-scrollbar-track { background: #E3E3E3; }
   ::-webkit-scrollbar-thumb { background: #C1C1C1; border-radius: 2px; }
